452381e191c1da7bcb316eb360f46267790a593a
adkasyv <kasymbekov01384@gmail.com>
2022-09-01 11:44:43 +0600

    Initial commit

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
-// const express = require("express");
-// const favicon = require("express-favicon");
-// const path = require("path");
-// const port = process.env.PORT || 8080;
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+const port = process.env.PORT || 8080;
 
-// // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
-// const app = express();
-// app.use(favicon(__dirname + "/build/favicon.png"));
+// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+const app = express();
+app.use(favicon(__dirname + "/build/favicon.png"));
 
-// //здесь наше приложение отдаёт статику
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, "build")));
+//здесь наше приложение отдаёт статику
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
 
-// //простой тест сервера
-// app.get("/ping", function (req, res) {
-//   return res.send("pong");
-// });
+//простой тест сервера
+app.get("/ping", function (req, res) {
+  return res.send("pong");
+});
 
-// //обслуживание html
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
-// app.listen(port);
-
-import { createServer } from "http";
-import { Server } from "node-static";
-var file = new Server(".");
-
-createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(8080);
-
-console.log("Server running on port 8080");
+//обслуживание html
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(port);
